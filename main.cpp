26eaee8fa39c0f0a16a3e4ff49933b35dd2c2993
superiorme <634206@dpsk12.net>
2017-04-26 08:33:10 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main() {
+	int input;
+		int inventory;
+		int yes;
+
+		inventory[5];
+		   cout << "do you want the magic sword" << endl;
+		   cin >> input;
+		   if (input.compare = yes) {
+			   cout << "magical sword" << endl;
+		   inventory[0] = "magic sword";
+	   }
+		   else
+			   cout << "not valid answer" << endl;
+
+
+		
+
+			 if (inventory[0] == "magic sword") {
+				 cout << "o.k.you win" << endl;
+			 }
+			else (inventory[0] = !"magic sword") {
+				cout << "you dead" << endl;
+			}
+
+
+
+		
+
+
+
+
+
+}
