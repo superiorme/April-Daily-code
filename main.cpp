4d9c6a56ff24d9a4a2b074c089888e47db6bed7d
superiorme <634206@dpsk12.net>
2017-04-26 08:34:01 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include<ctime>
+#include<cstdlib>
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int RandomNumber;
+	
+	srand(static_cast<unsigned int>(time(0)));
+	RandomNumber = rand();
+	int dice = (RandomNumber%100)+1;
+	dice;
+	void RandomNumber();
+}
+
+void RandomNumber() {
+	int number;
+
+	int RandomNumber = rand()100 + 1;
+	if (number < 50)
+		system("color 2E");
+
+
+}
